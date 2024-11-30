1fcc10f4f3f2e8d6cdbf9f1899963ea590d0e26c
MatGon's Websites Development <109096019+mwd-oficial@users.noreply.github.com>
2024-11-30 11:33:02 -0300

    atualizei

--- server.js
+++ server.js
@@ -1,25 +1,37 @@
 import express from "express";
 import { routes } from "./src/routes/usersRoutes.js";
-import cors from "cors";
+
+const allowCors = fn => async (req, res) => {
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    // outra configuração comum
+    // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+    res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT');
+    res.setHeader(
+        'Access-Control-Allow-Headers',
+        'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+    );
+    if (req.method === 'OPTIONS') {
+        res.status(200).end();
+        return;
+    }
+    return await fn(req, res);
+}
 
 const app = express();
-
-const corsOptions = {
-    origin: ["https://mwd-oficial.github.io", "http://127.0.0.1:5500"],
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    allowedHeaders: ["Content-Type"]
+const handler = (req, res) => {
+    const port = process.env.PORT || 3000;
+    app.listen(port, () => {
+        console.log(`Servidor escutando na porta ${port}`);
+    });
 };
 
-app.use(cors(corsOptions));
-app.options('*', cors(corsOptions));
 app.use(express.json());
 routes(app);
 
+// Rota padrão (fallback)
 app.use((req, res) => {
     res.status(404).send('Página não encontrada');
 });
 
-const port = process.env.PORT || 3000;
-app.listen(port, () => {
-    console.log(`Servidor escutando na porta ${port}`);
-});
+module.exports = allowCors(handler);
--- src/routes/usersRoutes.js
+++ src/routes/usersRoutes.js
@@ -2,33 +2,6 @@ import express from "express";
 import { listarUsers, cadastrarUsers, excluirUsers } from "../controllers/usersController.js";
 
 export function routes(app) {
-    app.use(express.json());
-    
-    const allowCors = fn => async (req, res) => {
-        res.setHeader('Access-Control-Allow-Credentials', true)
-        res.setHeader('Access-Control-Allow-Origin', '*')
-        // another common pattern
-        // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
-        res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
-        res.setHeader(
-          'Access-Control-Allow-Headers',
-          'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
-        )
-        if (req.method === 'OPTIONS') {
-          res.status(200).end()
-          return
-        }
-        return await fn(req, res)
-      }
-      
-      const handler = (req, res) => {
-        const d = new Date()
-        res.end(d.toString())
-      }
-      
-      module.exports = allowCors(handler)
-      
-
     app.get("/users", listarUsers);
     app.post("/users", cadastrarUsers);
     app.delete("/users", excluirUsers);
