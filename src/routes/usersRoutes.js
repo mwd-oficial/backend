2dfc6419d0a5e11cedd621f5c1a96e459928e0a1
MatGon's Websites Development <109096019+mwd-oficial@users.noreply.github.com>
2024-11-30 11:28:51 -0300

    Update usersRoutes.js

--- src/routes/usersRoutes.js
+++ src/routes/usersRoutes.js
@@ -4,12 +4,30 @@ import { listarUsers, cadastrarUsers, excluirUsers } from "../controllers/usersC
 export function routes(app) {
     app.use(express.json());
     
-    app.use((req, res, next) => {
-        res.header("Access-Control-Allow-Origin", "https://mwd-oficial.github.io", "http://127.0.0.1:5500", "http://localhost:3000"); // Adicione os domínios permitidos aqui
-        res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-        res.header("Access-Control-Allow-Headers", "Content-Type");
-        next();
-    });
+    const allowCors = fn => async (req, res) => {
+        res.setHeader('Access-Control-Allow-Credentials', true)
+        res.setHeader('Access-Control-Allow-Origin', '*')
+        // another common pattern
+        // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+        res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
+        res.setHeader(
+          'Access-Control-Allow-Headers',
+          'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+        )
+        if (req.method === 'OPTIONS') {
+          res.status(200).end()
+          return
+        }
+        return await fn(req, res)
+      }
+      
+      const handler = (req, res) => {
+        const d = new Date()
+        res.end(d.toString())
+      }
+      
+      module.exports = allowCors(handler)
+      
 
     app.get("/users", listarUsers);
     app.post("/users", cadastrarUsers);
