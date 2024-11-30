07f9483ef58226fcc1fd460064380769d7a5fb00
MatGon's Websites Development <109096019+mwd-oficial@users.noreply.github.com>
2024-11-30 11:51:49 -0300

    atualizei

--- server.js
+++ server.js
@@ -1,37 +1,9 @@
 import express from "express";
-import { routes } from "./src/routes/usersRoutes.js";
-
-const allowCors = fn => async (req, res) => {
-    res.setHeader('Access-Control-Allow-Credentials', true);
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    // outra configuração comum
-    // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
-    res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT');
-    res.setHeader(
-        'Access-Control-Allow-Headers',
-        'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
-    );
-    if (req.method === 'OPTIONS') {
-        res.status(200).end();
-        return;
-    }
-    return await fn(req, res);
-}
+import {routes} from "./src/routes/usersRoutes.js";
 
 const app = express();
-const handler = (req, res) => {
-    const port = process.env.PORT || 3000;
-    app.listen(port, () => {
-        console.log(`Servidor escutando na porta ${port}`);
-    });
-};
-
-app.use(express.json());
 routes(app);
 
-// Rota padrão (fallback)
-app.use((req, res) => {
-    res.status(404).send('Página não encontrada');
-});
-
-module.exports = allowCors(handler);
+app.listen(3000, () => {
+    console.log("Servidor escutando na porta 3000");
+})
--- src/routes/usersRoutes.js
+++ src/routes/usersRoutes.js
@@ -1,7 +1,17 @@
 import express from "express";
-import { listarUsers, cadastrarUsers, excluirUsers } from "../controllers/usersController.js";
+import cors from "cors";
+import { listarUsers, cadastrarUsers, excluirUsers } from "../controllers/usersController.js"
+
+const corsOptions = {
+    //origin: ["https://mwd-oficial.github.io", "http://127.0.0.1:5500"],
+    origin: "*",
+    optionsSuccessStatus: 200
+}
 
 export function routes(app) {
+    app.use(express.json());
+    app.use(cors(corsOptions))
+    app.options('/api/users', cors(corsOptions))
     app.get("/users", listarUsers);
     app.post("/users", cadastrarUsers);
     app.delete("/users", excluirUsers);
