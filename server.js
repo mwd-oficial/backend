bffc214ca357a915c1cbb6cb0222c1212f4347b7
MatGon's Websites Development <109096019+mwd-oficial@users.noreply.github.com>
2024-11-30 10:36:22 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,15 +1,9 @@
 import express from "express";
-import { routes } from "./src/routes/usersRoutes.js";
+import {routes} from "./src/routes/usersRoutes.js";
 
 const app = express();
 routes(app);
 
-// Rota padrão (fallback)
-app.use((req, res) => {
-    res.status(404).send('Página não encontrada');
-});
-
-const port = process.env.PORT || 3000;
-app.listen(port, () => {
-    console.log(`Servidor escutando na porta ${port}`);
-});
+app.listen(3000, () => {
+    console.log("Servidor escutando na porta 3000");
+})
