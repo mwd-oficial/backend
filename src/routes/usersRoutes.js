23c025ca6a40a6f5cde8ee39f0693e5851c7ded5
MatGon's Websites Development <109096019+mwd-oficial@users.noreply.github.com>
2024-11-29 22:44:03 -0300

    atualizei

--- src/routes/usersRoutes.js
+++ src/routes/usersRoutes.js
@@ -2,15 +2,15 @@ import express from "express";
 import cors from "cors";
 import { listarUsers, cadastrarUsers, excluirUsers } from "../controllers/usersController.js"
 
-const corsOption = {
-    //origin: ["https://mwd-oficial.github.io", "http://127.0.0.1:5500"],
-    origin: "*",
+const corsOptions = {
+    origin: ["https://mwd-oficial.github.io", "http://127.0.0.1:5500"],
     optionsSuccessStatus: 200
 }
 
 export function routes(app) {
     app.use(express.json());
-    app.use(cors(corsOption))
+    app.use(cors(corsOptions))
+    app.options('/api/users', cors(corsOptions))
     app.get("/users", listarUsers);
     app.post("/cadastrar.html", cadastrarUsers);
     app.delete("/users", excluirUsers);
