37a91e65841449e3976ba088a1e3a206d2203ecd
MatGon's Websites Development <109096019+mwd-oficial@users.noreply.github.com>
2024-11-30 10:46:36 -0300

    atualizei

--- server.js
+++ server.js
@@ -1,9 +1,26 @@
 import express from "express";
-import {routes} from "./src/routes/usersRoutes.js";
+import { routes } from "./src/routes/usersRoutes.js";
+import cors from "cors";
 
 const app = express();
+
+// Configuração do CORS
+const corsOptions = {
+    origin: ["https://mwd-oficial.github.io", "http://127.0.0.1:5500", "http://localhost:3000"],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type"]
+};
+
+app.use(cors(corsOptions));
+app.use(express.json());
 routes(app);
 
-app.listen(3000, () => {
-    console.log("Servidor escutando na porta 3000");
-})
+// Rota padrão (fallback)
+app.use((req, res) => {
+    res.status(404).send('Página não encontrada');
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log(`Servidor escutando na porta ${port}`);
+});
