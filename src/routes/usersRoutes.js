06123d3062a203e46edc6038e51458fc56411a45
MatGon's Websites Development <109096019+mwd-oficial@users.noreply.github.com>
2026-03-26 21:14:42 -0300

    fnafsbr pt1

--- src/routes/usersRoutes.js
+++ src/routes/usersRoutes.js
@@ -14,6 +14,7 @@ const upload = multer()
 
 export function routes(app) {
     app.use(express.json())
+    app.use(express.static('public'));
     app.use(cors(corsOptions));
     app.use((req, res, next) => {
         res.header('Access-Control-Allow-Origin', '*');
