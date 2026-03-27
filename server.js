23b25d2e428bf1a647be443996e7e9980ac5635e
MatGon's Websites Development <109096019+mwd-oficial@users.noreply.github.com>
2026-03-26 21:41:08 -0300

    atualizei

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ import express from "express";
 import { routes } from "./src/routes/usersRoutes.js";
 
 const app = express();
+app.use(express.static('public'));
 routes(app);
 
 const PORT = process.env.PORT || 3000
--- src/routes/usersRoutes.js
+++ src/routes/usersRoutes.js
@@ -14,7 +14,6 @@ const upload = multer()
 
 export function routes(app) {
     app.use(express.json())
-    app.use(express.static('public'));
     app.use(cors(corsOptions));
     app.use((req, res, next) => {
         res.header('Access-Control-Allow-Origin', '*');
